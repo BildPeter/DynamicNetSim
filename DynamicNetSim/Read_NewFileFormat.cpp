c4123a1be66ff3f09ece303135df7d8e6ee67815
BildPeter <BildPeter@web.de>
2012-12-13 18:36:41 +0100

    Created a class to activate and deactivate the temporal graph edges for one time step. Put it all into a separate file 

--- DynamicNetSim/Read_NewFileFormat.cpp
+++ DynamicNetSim/Read_NewFileFormat.cpp
@@ -17,11 +17,10 @@
 #include <unordered_map>
 #include <fstream>
 
+#include "temporal_graph_handler.h"
+
 using namespace std;
 using namespace lemon;
-
-template < typename DGR >
-vector<int> readTemporalArcList( DGR &graph, unordered_map< int, vector<typename DGR::Arc > > &Time_ToArcVec, string fn);
 
 int main(){
     
@@ -34,10 +33,12 @@ int main(){
     SmartDigraph                        mGraph;
 
     Timer T(true);
+    // --- read the LEMON graph
     digraphReader( mGraph, schweinLGF)
     .run();
 
-    activeTimes = readTemporalArcList<SmartDigraph>( mGraph, mTime_Arcs_Vec, schweinDynArcs);
+    // --- read
+    activeTimes = tempGR::readTemporalArcList<SmartDigraph>( mGraph, mTime_Arcs_Vec, schweinDynArcs);
     
     cout << "read Time: " << T.realTime() << endl;
     cout << "nodes: " << countNodes( mGraph ) << " Arcs: " << countArcs( mGraph ) << endl;
@@ -51,36 +52,33 @@ int main(){
     SmartDigraph::ArcMap<int>               arcCount( mGraph );
     SmartDigraph::NodeMap<int>              nodeCount( mGraph );
 
+    tempGR::temporalGraphActivator<SmartDigraph>        mActivator( mGraph, activeNodes, activeArcs, mTime_Arcs_Vec );
+    
     int calSteps = 0;
     
     T.restart();
     
     for ( auto currentTime : activeTimes) {
         
-        for ( auto arc : mTime_Arcs_Vec[ currentTime ] ){
-            activeArcs[ arc ]                   = true;
-            activeNodes[ mGraph.source( arc ) ] = true;
-            activeNodes[ mGraph.target( arc ) ] = true;
-        }
+        mActivator.activate( currentTime );
         
         for (SubDigraph<SmartDigraph>::NodeIt n(subSystem); n!=INVALID; ++n) {
             nodeCount[ n ]++;
             calSteps++;
         }
-
-        for ( auto arc : mTime_Arcs_Vec[ currentTime ] ){
-            activeArcs[ arc ]                   = false;
-            activeNodes[ mGraph.source( arc ) ] = false;
-            activeNodes[ mGraph.target( arc ) ] = false;
-        }
+        
+        mActivator.deactivate( currentTime );
+        
     }
     
     cout << "calculation Time: " << T.realTime() << endl;
     cout << "calculation Steps: " << calSteps << endl;
-    cout << nodeCount[ mGraph.nodeFromId( 2 ) ] << endl;
     cout << endl << "pause" << endl;
-//    usleep(10000000);
+    usleep(10000000);
     
+    //==================================================================================
+    //==================================================================================    
+
     InDegMap<SmartDigraph>      mInDegree(mGraph);
     OutDegMap<SmartDigraph>     mOutDegree(mGraph);
     
@@ -103,26 +101,3 @@ int main(){
     
     cout << "subGraph - nodes: " << countNodes( subSystem ) << " | arcs: " << countArcs( subSystem ) << endl;
  }
-
-
-template < typename DGR >
-vector<int> readTemporalArcList( DGR &graph, unordered_map< int, vector<typename DGR::Arc > > &Time_ToArcVec, string fn){
-    vector< int > activeTimes;
-    ifstream input( fn );
-    
-    int     time, arcID;
-    string tmpData;
-    getline( input, tmpData );  // Kill first line (header)
-    for (string line; getline(input, line, '\n'); ) {
-        istringstream lineStream(line);
-        lineStream >> time;     // -- read first number ---
-        activeTimes.push_back( time );
-        
-        for (; getline(lineStream, tmpData, '\t'); ) {
-            lineStream >> arcID;
-            Time_ToArcVec[ time ].push_back( graph.arcFromId( arcID ) );
-        }
-        Time_ToArcVec[time].pop_back();    // -- do not really know why, seems to read last element twice (not endet be \t)
-    }
-    return activeTimes;
-}

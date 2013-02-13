d20059e6baa470501a4309954dd34fe8a5c34d98
Peter A. Kolski <sonneundasche@client200-238.wlan.hu-berlin.de>
2013-02-13 12:56:11 +0100

    Program to calculate the degree and the amount of activity on each node of a TempGraph

---
+++ Process_DegreeActivity.cpp
@@ -0,0 +1,93 @@
+//
+//  Process_DegreeActivity.cpp
+//  DynamicNetSim
+//
+//  Created by Peter A. Kolski on 13.02.13.
+//  Copyright (c) 2013 Peter. All rights reserved.
+//
+
+/*
+    AIM:
+    Process Data to output a node list with the degree and nr of activities
+ 
+    INPUT:      2 files - LGF and tempEdges
+    OUTPUT:     NodeList with degree and activity (TimeSpan in Filename).txt
+ 
+    TODO:
+        - output filename with timespan (steps)
+        - LMap to cout activity
+ */
+
+#include <lemon/smart_graph.h>
+#include <lemon/arg_parser.h>
+#include <lemon/maps.h>
+#include <lemon/lgf_reader.h>
+#include <lemon/lgf_writer.h>
+
+#include <peter/temporal_graph_handler.h>
+#include <boost/unordered_map.hpp>
+
+using namespace lemon;
+using namespace std;
+
+int main(int argc, char** argv){
+
+    // ------------------------------
+    // --- INPUT - OUTPUT
+    string  sourceLGF, sourceTempEdge, target;
+    ArgParser       ap( argc, argv);
+    ap.refOption("g", "Graph file of LEMON GRAPH FORMAT", sourceLGF, true);
+    ap.refOption("t", "Temporal active edges", sourceTempEdge, true);
+    ap.refOption("n", "Target filename", target, true);
+    ap.parse();
+    
+//    sourceLGF       = "/Users/sonneundasche/Documents/FLI/DATA/02 Daten - Schaf/Schaf_NEW_.lgf";
+//    sourceTempEdge  = "/Users/sonneundasche/Documents/FLI/DATA/02 Daten - Schaf/Schaf_NEW__time_tmpArcIDs.txt";
+//    target          = "Test";
+
+    // ------------------------------
+    // --- Graph creation
+    SmartDigraph                    mGraph;
+    try {
+        digraphReader(mGraph, sourceLGF)
+        .run();
+    } catch (lemon::Exception) {
+        exit(0);
+    }
+    
+    SmartDigraph::NodeMap<int>      activity(mGraph);
+    InDegMap<SmartDigraph>          inDegree(mGraph);
+    OutDegMap<SmartDigraph>         outDegree(mGraph);
+    AddMap<InDegMap<SmartDigraph>, OutDegMap<SmartDigraph> > mDegree(inDegree, outDegree);
+    
+    
+    // ------------------------------
+    // --- Temporal Edges
+    vector<int> times;
+    boost::unordered_map< int,  vector<SmartDigraph::Arc > >   mTimeToArcs;
+    times = tempGR::readTemporalArcList(mGraph, mTimeToArcs, sourceTempEdge);
+    int timeSteps = times.size();
+    
+    // ------------------------------
+    // --- Count the activity
+    for ( auto t : times){
+        for (auto arc :mTimeToArcs[t]){
+            activity[ mGraph.source( arc ) ]++;
+            activity[ mGraph.target( arc ) ]++;
+        }
+    }
+    
+    // --- Filename: add the nr of timesteps
+    target += "_";
+    stringstream ss;
+    ss << timeSteps;
+    target += ss.str();
+    target += "steps.txt";
+    
+    digraphWriter(mGraph, target)
+    .nodeMap("degree", mDegree)
+    .nodeMap("activity", activity)
+    .skipArcs()
+    .run();
+    
+}
